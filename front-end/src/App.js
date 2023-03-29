a54818ebddda63419a8126bf2f8f030f5e65d52b
Leonardo Selegar <leonardo11132@gmail.com>
2023-03-29 18:28:24 -0300

    Fix: Login router

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import { Switch, Route, Navigate } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Login from './pages/Login';
 
 function App() {
   return (
     <Switch>
-      <Route exact path="/" element={ <Navigate to="/login" /> } />
+      <Route exact path="/" element={ <Redirect to="/login" /> } />
       <Route exact path="/login" element={ <Login /> } />
     </Switch>
   );

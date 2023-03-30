c65bdc4b34fb0043b218d9fbb9839c50d88df8a3
Karin Melo <karinsantosmelo@gmail.com>
2023-03-30 18:08:57 -0300

    backend-login

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -3,5 +3,6 @@ const express = require('express');
 const app = express();
 
 app.get('/coffee', (_req, res) => res.status(418).end());
+app.post('/login');
 
 module.exports = app;
---
+++ back-end/src/api/services/loginService.js
@@ -0,0 +1,37 @@
+/* import User */ // ver nome da model depois para alterar
+const crypto = require('crypto');
+
+const login = async (body) => {
+const { email, password } = body;
+const hashedPassword = crypto.createHash('md5').update(password).digest('hex')
+const result = await User.findOne({ where: { email, password: hashedPassword } });
+if(!result) return { type: 'error', message: 'User not found' }
+const { dataValues } = result;
+return { type: null, message: dataValues };
+}
+
+module.exports = login;
+
+
+/* 8 - Desenvolva a tela de registro de maneira que ela impossibilite o cadastro com dados mal formatados
+Observações técnicas
+
+A senha recebe qualquer tipo de caractere;
+Aqui, os critérios para considerar os dados mal formatados são:
+Nome completo com número de caracteres menor que 12.
+Email incompleto, fora de um padrão comum: <email>@<domínioPrincipal>.<domínioGenérico>;
+Senha com número de caracteres menor que 6.
+O que será avaliado
+9 - Desenvolva a tela de registro de maneira que ela possibilite cadastrar com dados válidos
+Observações técnicas
+
+Sua página deve ser capaz de cadastrar pessoas usuárias com dados válidos:
+
+Note que a senha deve ser armazenada no banco como uma hash md5. A tradução deve ocorrer na API;
+É possível utilizar bibliotecas de terceiros como a md5 ou a nativa crypto, para conversão de valores para md5.
+O que será avaliado
+10 - Desenvolva a tela de registro de maneira que ela impossibilite o cadastro de um usuário já existente
+Observações técnicas
+
+Sua página deve impedir o cadastro de pessoas com o mesmo Nome ou E-mail.
+O que será avaliado */

f57e128606f36fc72bd3d782b8c3d5a062f4c0f3
Giordano <giordano.mendes94@gmail.com>
2023-04-04 09:33:04 -0300

    feat: route Register and CustomerProducts

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Login from './pages/Login';
+import Register from './pages/Register';
+import CustomerProducts from './pages/CustomerProducts';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" render={ (props) => <Redirect to="/login" { ...props } /> } />
       <Route exact path="/login" component={ Login } />
+      <Route exact path="/register" component={ Register } />
+      <Route exact path="/customer/products" component={ CustomerProducts } />
     </Switch>
   );
 }

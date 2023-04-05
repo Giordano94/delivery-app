6e19b2c6e866a7c1c8196a9bd467171f3d60f1e6
Giordano <giordano.mendes94@gmail.com>
2023-04-05 19:26:54 -0300

    feat: route customerCheckout

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,14 +4,23 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import CustomerProducts from './pages/CustomerProducts';
+import ProductProvider from './context/Provider';
+import CustomerCheckout from './pages/CustomerCheckout';
 
 function App() {
   return (
     <Switch>
-      <Route exact path="/" render={ (props) => <Redirect to="/login" { ...props } /> } />
+      <Route
+        exact
+        path="/"
+        render={ (props) => <Redirect to="/login" { ...props } /> }
+      />
       <Route exact path="/login" component={ Login } />
       <Route exact path="/register" component={ Register } />
-      <Route exact path="/customer/products" component={ CustomerProducts } />
+      <ProductProvider>
+        <Route exact path="/customer/products" component={ CustomerProducts } />
+      </ProductProvider>
+      <Route exact path="/customer/checkout" component={ CustomerCheckout } />
     </Switch>
   );
 }

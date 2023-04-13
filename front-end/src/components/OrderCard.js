2fab82454b7a63f5c1e8922060d94eac10693ccd
Giordano <giordano.mendes94@gmail.com>
2023-04-13 18:51:21 -0300

    refactor: format date with moment

--- front-end/src/components/OrderCard.js
+++ front-end/src/components/OrderCard.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { useHistory } from 'react-router-dom';
+import moment from 'moment';
 import ProductContext from '../context/Context';
 
 function OrderCard({ order }) {
@@ -8,17 +9,11 @@ function OrderCard({ order }) {
   const { setOrderGlobal } = useContext(ProductContext);
   const history = useHistory();
 
-  const dateHour = new Date(saleDate);
-  const number2 = -2;
-  const day = dateHour.getDate().toString().padStart(2, '0');
-  const month = (dateHour.getMonth() + 1).toString().padStart(2, '0');
-  const year = dateHour.getFullYear().toString().substr(number2);
-  const formatDate = `${day}/${month}/${year}`;
-
   const handleClick = () => {
     setOrderGlobal(order);
     history.push(`/customer/orders/${id}`);
   };
+  console.log(id);
 
   return (
     <button type="button" onClick={ handleClick }>
@@ -32,10 +27,10 @@ function OrderCard({ order }) {
           {status}
         </h1>
         <h2 data-testid={ `customer_orders__element-order-date-${id}` }>
-          {formatDate}
+          {moment(saleDate).format('DD/MM/YYYY')}
         </h2>
         <h2 data-testid={ `customer_orders__element-card-price-${id}` }>
-          {totalPrice.replace('.', ',')}
+          {totalPrice?.replace('.', ',')}
         </h2>
       </div>
     </button>

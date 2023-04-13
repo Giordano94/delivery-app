3b7f428c24a9a52a552adaafe9325017de9aa25f
Giordano <giordano.mendes94@gmail.com>
2023-04-13 18:51:56 -0300

    fix: id by orders

--- front-end/src/pages/CustomerOrder.js
+++ front-end/src/pages/CustomerOrder.js
@@ -1,17 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import OrderCard from '../components/OrderCard';
-import { reqLogin, reqOrders } from '../services/apiRequest';
+import { reqLogin, postRegister } from '../services/apiRequest';
 
 export default function CustomerOrders() {
   const [pedidos, setPedidos] = useState([]);
+  console.log(pedidos);
 
   useEffect(() => {
     const req = async () => {
       const { email } = JSON.parse(localStorage.getItem('user'));
       const user = await reqLogin('/user', { email });
       const { id } = user[0];
-      const pedidosArray = await reqOrders(`/orders/${id}`);
+      console.log(id);
+      const pedidosArray = await postRegister('/orders', { userId: id });
+      console.log('pedidosArray', pedidosArray);
       setPedidos(pedidosArray);
     };
     req();

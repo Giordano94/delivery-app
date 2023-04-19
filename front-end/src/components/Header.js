d8482d42ea6a828940b459a16bc871d18d85d188
Karin Melo <karinsantosmelo@gmail.com>
2023-04-19 06:12:13 +0000

    Correção 2 de lint

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -2,81 +2,63 @@ import PropTypes from 'prop-types';
 import React, { useEffect, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 
-function Header({ showProdutos = true, showGerenciar = true }) {
-  function Header({
-    showProdutos = true,
-    showMeusPedidos = true,
-    showName = true,
-  }) {
-    const history = useHistory();
-    const [nome, setNome] = useState('');
-    let linkOrders = '';
+function Header({
+  showProdutos = true,
+  showMeusPedidos = true,
+  showName = true,
+}) {
+  const history = useHistory();
+  const [nome, setNome] = useState('');
 
-    useEffect(() => {
-      const { name } = JSON.parse(localStorage.getItem('user'));
-      setNome(name);
+  useEffect(() => {
+    const { name } = JSON.parse(localStorage.getItem('user'));
+    setNome(name);
+  });
 
-      if (showGerenciar) linkOrders = 'GERENCIAR USUARIOS';
-      if (showProdutos) linkOrders = 'MEUS PEDIDOS';
-    });
+  const logout = () => {
+    localStorage.clear();
+    history.push('/login');
+  };
 
-    const logout = () => {
-      localStorage.clear();
-      history.push('/login');
-    };
-
-    return (
-      <div>
-        {showProdutos && (
-          <button
-            data-testid="customer_products__element-navbar-link-products"
-            type="button"
-          >
-            PRODUTOS
-          </button>
-        )}
-
+  return (
+    <div>
+      {showProdutos && (
         <button
-          data-testid="customer_products__element-navbar-link-orders"
+          data-testid="customer_products__element-navbar-link-products"
           type="button"
         >
-          {linkOrders}
+          PRODUTOS
         </button>
+      )}
+      {showMeusPedidos && (
+        <Link to="/customer/orders">
+          <button
+            data-testid="customer_products__element-navbar-link-orders"
+            type="button"
+          >
+            MEUS PEDIDOS
+          </button>
+        </Link>
+      )}
+      {showName && (
         <button
           data-testid="customer_products__element-navbar-user-full-name"
           type="button"
         >
           {nome}
         </button>
-        {showMeusPedidos && (
-          <Link to="/customer/orders">
-            <button
-              data-testid="customer_products__element-navbar-link-orders"
-              type="button"
-            >
-              MEUS PEDIDOS
-            </button>
-          </Link>
-        )}
-        {showName && (
-          <button
-            data-testid="customer_products__element-navbar-user-full-name"
-            type="button"
-          >
-            {nome}
-          </button>
-        )}
-        <button
-          data-testid="customer_products__element-navbar-link-logout"
-          type="button"
-          onClick={logout}
-        >
-          Sair
-        </button>
-      </div>
-    );
-  }
+      )}
+      <button
+        data-testid="customer_products__element-navbar-link-logout"
+        type="button"
+        onClick={ logout }
+      >
+        Sair
+      </button>
+    </div>
+  );
 }
+
 Header.propTypes = {
   showProdutos: PropTypes.bool,
   showMeusPedidos: PropTypes.bool,

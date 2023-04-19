fee3a9c706bbf453605fe7fd013351b0002bd940
Giordano <giordano.mendes94@gmail.com>
2023-04-19 11:41:11 -0300

    refactor: remove `console.log()` from CustomerOrders page

--- front-end/src/pages/CustomerOrder.js
+++ front-end/src/pages/CustomerOrder.js
@@ -5,16 +5,13 @@ import { reqLogin, postRegister } from '../services/apiRequest';
 
 export default function CustomerOrders() {
   const [pedidos, setPedidos] = useState([]);
-  console.log(pedidos);
 
   useEffect(() => {
     const req = async () => {
       const { email } = JSON.parse(localStorage.getItem('user'));
       const user = await reqLogin('/user', { email });
       const { id } = user[0];
-      console.log(id);
       const pedidosArray = await postRegister('/orders', { userId: id });
-      console.log('pedidosArray', pedidosArray);
       setPedidos(pedidosArray);
     };
     req();

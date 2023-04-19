f7997e43cb8da6bde10be584b64d0cc4ce71f70e
Giordano <giordano.mendes94@gmail.com>
2023-04-19 11:27:43 -0300

    feat: adds redirection based on user role on login page

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { useHistory, Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { reqLogin, reqToken } from '../services/apiRequest';
+import '../styles/LoginCss.css';
 
 function Login() {
   const history = useHistory();
@@ -18,11 +19,21 @@ function Login() {
   const limitator = 6;
   let stringEmail = email.match(/\S+@\S+\.\S+/i);
 
-  useEffect(() => {
+  const redirect = () => {
     const userExists = JSON.parse(localStorage.getItem('user'));
-    if (userExists) {
-      history.push('/customer/products');
+    if (userExists !== null) {
+      if (userExists.role === 'customer') {
+        history.push('/customer/products');
+      } else if (userExists.role === 'seller') {
+        history.push('/seller/orders');
+      } else if (userExists.role === 'administrator') {
+        history.push('/admin/manage');
+      }
     }
+  };
+
+  useEffect(() => {
+    redirect();
   }, []);
 
   useEffect(() => {
@@ -39,29 +50,45 @@ function Login() {
 
       localStorage.setItem('user', JSON.stringify(token));
 
-      history.push('/customer/products');
+      const loggedUser = JSON.parse(localStorage.getItem('user'));
+      const { role } = loggedUser;
+      if (role === 'customer') {
+        history.push('/customer/products');
+      } else if (role === 'seller') {
+        history.push('/seller/orders');
+      }
     } catch (error) {
       console.log(error);
       setIsValidation(true);
     }
   };
   return (
-    <div>
-      <form>
-        <input
-          type="email"
-          data-testid={ `${commonLogin}${inputEmail}` }
-          value={ email }
-          onChange={ (e) => setEmail(e.target.value) }
-        />
-        <input
-          type="password"
-          data-testid={ `${commonLogin}${inputPassword}` }
-          value={ password }
-          onChange={ (e) => setPassword(e.target.value) }
-        />
-
+    <div className="login-page">
+      <form className="form-login">
+        <div className="input-email">
+          Login
+          <input
+            className="input"
+            type="email"
+            placeholder="Digite seu email:"
+            data-testid={ `${commonLogin}${inputEmail}` }
+            value={ email }
+            onChange={ (e) => setEmail(e.target.value) }
+          />
+        </div>
+        <div className="input-senha">
+          Senha
+          <input
+            className="input"
+            type="password"
+            placeholder="Digite sua senha:"
+            data-testid={ `${commonLogin}${inputPassword}` }
+            value={ password }
+            onChange={ (e) => setPassword(e.target.value) }
+          />
+        </div>
         <button
+          className="button-entrar"
           type="button"
           data-testid={ `${commonLogin}${buttonLogin}` }
           disabled={ !(stringEmail && password.length >= limitator) }
@@ -69,11 +96,14 @@ function Login() {
         >
           LOGIN
         </button>
-        <Link to="/register">
-          <button type="button" data-testid={ `${commonLogin}${buttonRegister}` }>
-            I dont have a record
-          </button>
-        </Link>
+        <button
+          onClick={ () => history.push('/register') }
+          type="button"
+          className="button-cadastrar"
+          data-testid={ `${commonLogin}${buttonRegister}` }
+        >
+          Ainda não tenho conta
+        </button>
       </form>
       <p
         data-testid={ `${commonLogin}${elementInvalidEmail}` }

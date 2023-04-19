82042af36a6f42e4b0fb4bc4b212405da823d854
Giordano <giordano.mendes94@gmail.com>
2023-04-19 12:08:05 -0300

    feat: add getSellerOrderById funtion to controller

--- back-end/src/api/controllers/saleController.js
+++ back-end/src/api/controllers/saleController.js
@@ -4,10 +4,21 @@ const createSale = async (request, response) => {
   const { message } = await saleService.createSale(request.body);
   return response.status(201).json(message);
 };
+
 const getOrderById = async (request, response) => {
   const { userId } = request.body;
-  
+
   const { type, message } = await saleService.getOrderById(userId);
+  if (type) return response.status(404).json(message);
+
+  return response.status(200).json(message);
+};
+
+const getSellerOrderById = async (request, response) => {
+  const { sellerId } = request.body;
+  console.log('sellerId', sellerId);
+
+  const { type, message } = await saleService.getSellerOrderById(sellerId);
   if (type) return response.status(404).json(message);
 
   return response.status(200).json(message);
@@ -15,10 +26,11 @@ const getOrderById = async (request, response) => {
 
 const getSaleById = async (request, response) => {
   const { id } = request.params;
-  
+
   const { type, message } = await saleService.getSaleById(id);
   if (type) return response.status(404).json(message);
 
   return response.status(200).json(message);
 };
-module.exports = { createSale, getSaleById, getOrderById };
+
+module.exports = { createSale, getSaleById, getOrderById, getSellerOrderById };
